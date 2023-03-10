dd6d5abdff02ebe46df910be16ed85d3a2e1a888
protondk <edgarjosue2911@gmail.com>
2023-03-09 18:41:52 -0600

    Practiced map method

---
+++ map.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3, 4, 5];
+const names = ["Edgar", "Josue", "Torres", "Urquilla"];
+
+console.log(numbers.map((num) => num * num));
+console.log(names.map((names) => names.toUpperCase()));
