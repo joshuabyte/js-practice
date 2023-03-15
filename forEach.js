88e9e5a9389ce502293b8948ce81fa018ee5e374
protondk <edgarjosue2911@gmail.com>
2023-03-15 10:26:24 -0600

    Practiced forEach with cars array

--- forEach.js
+++ forEach.js
@@ -1,5 +1,10 @@
 /* Adding a new property to each object in the array. */
 //forEach method is adding the new property isAdult. The new property will be equal to true or false depending on the users age.
+const fruits = ["Apples", "Oranges", "Bananas", "Grapes"];
+
+fruits.push("Tangerine");
+console.log(fruits);
+
 const users = [
   {
     name: "Edgar",
@@ -33,4 +38,23 @@ myStudents.forEach((student) => {
   student.isApproved = student.age >= 18;
 });
 
-console.log(myStudents)
+console.log(myStudents);
+
+const cars = [
+  { brand: "Toyota", model: "Camry", year: 2021 },
+  { brand: "Honda", model: "Civic", year: 2022 },
+  { brand: "Ford", model: "Mustang", year: 2019 },
+  { brand: "Tesla", model: "Model S", year: 2020 },
+  { brand: "Chevrolet", model: "Corvette", year: 2023 },
+  { brand: "BMW", model: "M3", year: 2018 },
+  { brand: "Mercedes-Benz", model: "C-Class", year: 2021 },
+  { brand: "Audi", model: "A4", year: 2022 },
+  { brand: "Lamborghini", model: "Huracan", year: 2021 },
+  { brand: "Ferrari", model: "488 GTB", year: 2019 },
+];
+
+cars.forEach((car) => {
+  car.hasInsurance = Math.round(Math.random()) ? "Yes" : "No";
+});
+
+console.log(cars);
